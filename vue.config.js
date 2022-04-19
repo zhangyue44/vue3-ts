e7daf7015c00a06bb0e9243a14c8700845e9b0e1
zhangyue44 <zy1617768774@163.com>
2022-04-20 01:17:56 +0800

    fix: add plugins

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,7 @@ const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: (config) => {
-    plugins: [
+    [
       AutoImport({
         rresolvers: [ElementPlusResolver()],
       }),
